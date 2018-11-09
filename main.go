212e4e778d7c5a2a17202ad54dbe16521f018d1f
Ray Pereda <raypereda@gmail.com>
2018-11-08 22:41:00 -0800

    Polish

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 
 // main is a utility that sort the standard input lines by length and then alphabetically.
 func main() {
+	if len(os.Args) != 1 {
+		fmt.Println("Usage: lensort")
+		fmt.Println("Sorts standard input by line length and alphabetically")
+		fmt.Println("Example: cat words.txt | lensort")
+		os.Exit(1)
+	}
+
 	var lines []string
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
